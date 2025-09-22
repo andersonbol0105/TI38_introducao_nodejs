b0163d11575c10314ed3f7636e0b861dcdab021b
ANDERSON APARECIDO BOLDINO <anderson.aboldino@senacsp.edu.br>
2025-09-22 08:59:41 -0300

    Criação de rota POST e GET para a tabela Disciplinas

--- expressMySQL/index.js
+++ expressMySQL/index.js
@@ -9,17 +9,35 @@ const BASEDIR= path.join(__dirname, 'templates')
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
+
+
+//Inserindo novo Cadastro
 app.get('/', (req, res)=>res.sendFile(`${BASEDIR}/index.html`))
-app.get('/cadastrar', (req, res)=>res.sendFile(`${BASEDIR}/cadastrar.html`))
+app.get('/cadastrar-alunos', (req, res)=>res.sendFile(`${BASEDIR}/cadastrar_alunos.html`))
+app.get('/cadastrar-disciplinas', (req, res)=>res.sendFile(`${BASEDIR}/cadastrar_disciplinas.html`))
+
 
 //CRUD
 //C - Inserir registro Aluno
-app.post('/cadastrar', (req, res)=>{
+app.post('/alunos', (req, res)=>{
     let dados = req.body
     dados = [dados.nome, parseFloat(dados.nota1), parseFloat(dados.nota2), parseFloat(dados.nota3), parseFloat(dados.nota4)]
     let sql = `
                 INSERT INTO alunos (nome, nota1, nota2, nota3, nota4)
                 VALUES (?, ?, ?, ?, ?);
+    `
+    con.query(sql, dados, (err, resp)=>{
+        let resposta
+        if(err) resposta = {...err, status:400, message: `Os dados não foram gravados`}
+        else resposta = {...resp, status:201, message:`Gravado com sucesso! - ${resp.affectedRows} linha(s) afetada(s)`}
+        res.json(resposta)
+    })
+})
+// C - Criar disciplina
+app.post('/disciplinas', (req, res) => {
+    let dados = req.body;
+    dados =  [dados.nome, dados.professor]
+    let sql = `INSERT INTO disciplinas (nome, professor) VALUES (?, ?);
     `
     con.query(sql, dados, (err, resp)=>{
         let resposta
@@ -39,6 +57,17 @@ app.get('/alunos', (req, res)=>{
         res.json(resposta)
     })
 })
+app.get('/disciplinas', (req, res)=>{
+    let sql = `SELECT* FROM disciplinas;`
+    con.query(sql, (err, resp)=>{
+        let resposta
+        if (err) resposta = {...err, status:400}
+        else resposta = {...resp, status:200}
+        res.json(resposta)
+    })
+})
+
+
 
 // U - Atualização
 app.patch('/alunos/:id', (req, res)=>{
@@ -56,6 +85,22 @@ app.patch('/alunos/:id', (req, res)=>{
     })
 })
 
+app.patch('/disciplinas/:id', (req, res)=>{
+    let dados = req.body
+    dados =  [(dados.nome), (dados.professor)]
+    let id = parseInt(req.params.id)
+    let sql = `UPDATE express_ti38.disciplinas
+    SET nome=?, professor=?, 
+    WHERE id=${id};`
+    con.query(sql, dados, (err, resp)=>{
+        let resposta
+        if (err) resposta = {...err, status:400, message:'Falha na atualização!'}
+        else resposta = {...resp, status:200, message:`Registro atualizado com sucesso!`}
+        res.json(resposta)
+    })
+})
+
+
 // D - Excluir registros
 app.delete('/alunos/:id', (req, res)=>{
     let id = parseInt(req.params.id)
@@ -67,6 +112,19 @@ app.delete('/alunos/:id', (req, res)=>{
         res.json(resposta)
     })
 })
+
+app.delete('/disciplinas/:id', (req, res)=>{
+    let id = parseInt(req.params.id)
+    let sql = `DELETE FROM disciplinas WHERE id=${id};`
+    con.query(sql,(err, resp)=>{
+        let resposta
+        if (err) respota = {...err, status:400, message:'Erro na exclusão de dados.'}
+        else resposta = {...resp, status:200, message:'Registro exclído com sucesso!'}
+        res.json(resposta)
+    })
+})
+
+
 
 //Alteração de teste
 
